b35606c31471eff3dcf532fba7802617e629f184
Nazrililyas123 <111737041+Nazrililyas123@users.noreply.github.com>
2022-10-09 16:25:44 -0700

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int angka[] ={3,5,6,8,12};
+	cout<< "angka" << angka[4]<<endl;
+	char huruf [] = {'x','y','z'};
+	printf("huruf %c",huruf[1]);
+	string nama []={"bagas","tanaka","panca"};
+	cout << "nama = "<< nama [0] <<endl;
+	
+	int nilai [3][5]=
+	{
+	    {10,20,30,40},
+	    {50,60,70,80},
+	    {90,100}
+};
+cout << "nilai" << nilai[1][3]<<endl;	
+
+int x=0;
+for(x;x<=5;x++){
+	cout << angka [x]<<endl;
+}
+}
